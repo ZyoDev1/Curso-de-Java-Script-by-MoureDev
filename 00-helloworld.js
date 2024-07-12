408253f6371cd2eab3842a575b00595afaeae747
zyo23 <jsebastiansoriano@gmail.com>
2024-07-12 17:59:28 +0200

    Initial commit

---
+++ 00-helloworld.js
@@ -0,0 +1,22 @@
+//Esto es un comentario simple
+
+/*Esto es 
+un comentario en varias lineas*/
+
+
+//Las tres formas de representar Hola Mundo
+console.log("Hola, JavaScript")
+console.log('Hola, JavaScript')
+console.log(`Hola, JavaScript`)
+
+/*console.log(`Hola, JavaScript`) Podemos convertir una función en comentario 
+console.log(`Hola, JavaScript`)
+console.log(`Hola, JavaScript`)*/
+
+console.log(5)
+console.log("5")
+console.log(5+2)
+console.log(5-2)
+console.log(5*2)
+console.log(5**2)//Corresponde a 5 elevado a 2
+console.log(5%2)//Averiguamos el resto de 5/2
---
+++ 01-variables.js
@@ -0,0 +1,28 @@
+//Empezamos a almacenar en memoria valores y referencias para trabajar con ellos.
+
+//Formas de crear variables en JS (let, var, const)
+
+
+//var
+var helloWorld = "Hello World" // El método var es el método que NO debemos utilizar.
+console.log(helloWorld)
+
+helloWorld="Hola de nuevo, JavaScript"
+console.log(helloWorld)//Hemos modificado el valor de la variable
+
+//let
+
+let helloWorld2 = "Hello World2" // El método var es el método que NO debemos utilizar.
+console.log(helloWorld2)
+
+helloWorld2="Hola de nuevo, JavaScript2"
+console.log(helloWorld2)//Hemos modificado el valor de la variable
+
+
+//const
+
+const helloWorld3 = "Hello helloWorld3" // El método var es el método que NO debemos utilizar.
+console.log(helloWorld3)
+
+// helloWorld3="Hola de nuevo, JavaScript3" [No se puede reasignar el valor a una variable definida por const ]
+console.log(helloWorld3)  
