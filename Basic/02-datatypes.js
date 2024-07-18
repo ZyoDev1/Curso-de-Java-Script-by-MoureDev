4b4b053f368a6930bdcaf7f3458fa1dc6ae0e752
zyo23 <jsebastiansoriano@gmail.com>
2024-07-18 18:34:19 +0200

    Clase 17/07/24

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,19 @@
+// Tipos de datos
+
+// Primitivos: son los datos basicos e inmutables y reprensentan un solo valor, y no tienen funciones o métodos.
+
+// String (Cadena de texto)
+
+let name = "Jorge Sebastián"
+let alias = 'ZyoDev'
+let email = `Mi email es jsebastiansoriano@gmail.com`
+
+// Number (Número)
+
+let age=32 //Entero 
+let height=1.75 //Decimal 
+
+// Boolean (booleanos)
+
+let isStudent = false
+let isTeacher = true
